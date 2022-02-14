b8cbe07e0a55c400e9b11678aaa1d3a8fb6942e1
Azie Melza Pratama <aziemp66@gmail.com>
2022-02-14 18:25:05 +0700

    New Project

--- data/database.js
+++ data/database.js
@@ -1,29 +1,29 @@
-const mongodb = require("mongodb");
+const mongodb = require('mongodb');
 
 const MongoClient = mongodb.MongoClient;
 
-let mongodbUrl = "mongodb://localhost:27017";
+let mongodbUrl = 'mongodb://localhost:27017';
 
 if (process.env.MONGODB_URL) {
-    mongodbUrl = process.env.MONGODB_URL; //IgqoxG5XwTzjJblG
+  mongodbUrl = process.env.MONGODB_URL; 
 }
 
 let database;
 
 async function initDatabase() {
-    const client = await MongoClient.connect(mongodbUrl);
-    database = client.db("deployment");
+  const client = await MongoClient.connect(mongodbUrl);
+  database = client.db('deployment');
 }
 
 function getDb() {
-    if (!database) {
-        throw new Error("No database connected!");
-    }
+  if (!database) {
+    throw new Error('No database connected!');
+  }
 
-    return database;
+  return database;
 }
 
 module.exports = {
-    initDatabase: initDatabase,
-    getDb: getDb,
+  initDatabase: initDatabase,
+  getDb: getDb,
 };
